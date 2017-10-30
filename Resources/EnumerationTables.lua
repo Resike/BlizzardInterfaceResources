e3935c86d6bedb32dbe8e8c170f4f2dca3aad9f3
Resike <resike@gmail.com>
2017-10-30 20:09:46 +0100

    7.3.2 (25383)

--- Resources/EnumerationTables.lua
+++ Resources/EnumerationTables.lua
@@ -1,209 +1,163 @@
-Enum = { }
+Enum.BattlepayBoostProduct.Level90Boost = 1
+Enum.BattlepayBoostProduct.Level100Boost = 11
 
-Enum.BattlepayBoostProduct = {
-	Level90Boost = 1,
-	Level100Boost = 11,
-}
+Enum.BattlepayBoostProductMeta.NumValues = 2
+Enum.BattlepayBoostProductMeta.MinValue = 0
+Enum.BattlepayBoostProductMeta.MaxValue = 11
 
-Enum.BattlepayBoostProductMeta = {
-	NumValues = 2,
-	MinValue = 0,
-	MaxValue = 11,
-}
+Enum.BattlepayProductDecorator.Boost = 0
+Enum.BattlepayProductDecorator.Expansion = 1
+Enum.BattlepayProductDecorator.WoWToken = 2
+Enum.BattlepayProductDecorator.VasService = 3
 
-Enum.BattlepayProductDecorator = {
-	Boost = 0,
-	Expansion = 1,
-	WoWToken = 2,
-	VasService = 3,
-}
+Enum.BattlepayProductDecoratorMeta.NumValues = 4
+Enum.BattlepayProductDecoratorMeta.MinValue = 0
+Enum.BattlepayProductDecoratorMeta.MaxValue = 3
 
-Enum.BattlepayProductDecoratorMeta = {
-	NumValues = 4,
-	MinValue = 0,
-	MaxValue = 3,
-}
+Enum.BattlepaySpecialProducts.Legion = 12
 
-Enum.BattlepaySpecialProducts = {
-	Legion = 12,
-}
+Enum.BattlepaySpecialProductsMeta.NumValues = 1
+Enum.BattlepaySpecialProductsMeta.MinValue = 0
+Enum.BattlepaySpecialProductsMeta.MaxValue = 12
 
-Enum.BattlepaySpecialProductsMeta = {
-	NumValues = 1,
-	MinValue = 0,
-	MaxValue = 12,
-}
+Enum.ContributionResult.Success = 0
+Enum.ContributionResult.MustBeNearNpc = 1
+Enum.ContributionResult.IncorrectState = 2
+Enum.ContributionResult.InvalidID = 3
+Enum.ContributionResult.QuestDataMissing = 4
+Enum.ContributionResult.FailedConditionCheck = 5
+Enum.ContributionResult.UnableToCompleteTurnIn = 6
+Enum.ContributionResult.InternalError = 7
 
-Enum.ContributionResult = {
-	Success = 0,
-	MustBeNearNpc = 1,
-	IncorrectState = 2,
-	InvalidID = 3,
-	QuestDataMissing = 4,
-	FailedConditionCheck = 5,
-	UnableToCompleteTurnIn = 6,
-	InternalError = 7,
-}
+Enum.ContributionResultMeta.NumValues = 8
+Enum.ContributionResultMeta.MinValue = 0
+Enum.ContributionResultMeta.MaxValue = 7
 
-Enum.ContributionResultMeta = {
-	NumValues = 8,
-	MinValue = 0,
-	MaxValue = 7,
-}
+Enum.ContributionState.None = 0
+Enum.ContributionState.Building = 1
+Enum.ContributionState.Active = 2
+Enum.ContributionState.UnderAttack = 3
+Enum.ContributionState.Destroyed = 4
 
-Enum.ContributionState = {
-	None = 0,
-	Building = 1,
-	Active = 2,
-	UnderAttack = 3,
-	Destroyed = 4,
-}
+Enum.ContributionStateMeta.NumValues = 5
+Enum.ContributionStateMeta.MinValue = 0
+Enum.ContributionStateMeta.MaxValue = 4
 
-Enum.ContributionStateMeta = {
-	NumValues = 5,
-	MinValue = 0,
-	MaxValue = 4,
-}
+Enum.PowerType.HealthCost = -2
+Enum.PowerType.None = -1
+Enum.PowerType.Mana = 0
+Enum.PowerType.Rage = 1
+Enum.PowerType.Focus = 2
+Enum.PowerType.Energy = 3
+Enum.PowerType.ComboPoints = 4
+Enum.PowerType.Runes = 5
+Enum.PowerType.RunicPower = 6
+Enum.PowerType.SoulShards = 7
+Enum.PowerType.LunarPower = 8
+Enum.PowerType.HolyPower = 9
+Enum.PowerType.Alternate = 10
+Enum.PowerType.Maelstrom = 11
+Enum.PowerType.Chi = 12
+Enum.PowerType.Insanity = 13
+Enum.PowerType.Obsolete = 14
+Enum.PowerType.Obsolete2 = 15
+Enum.PowerType.ArcaneCharges = 16
+Enum.PowerType.Fury = 17
+Enum.PowerType.Pain = 18
+Enum.PowerType.NumPowerTypes = 19
 
-Enum.PowerType = {
-	HealthCost = -2,
-	None = -1,
-	Mana = 0,
-	Rage = 1,
-	Focus = 2,
-	Energy = 3,
-	ComboPoints = 4,
-	Runes = 5,
-	RunicPower = 6,
-	SoulShards = 7,
-	LunarPower = 8,
-	HolyPower = 9,
-	Alternate = 10,
-	Maelstrom = 11,
-	Chi = 12,
-	Insanity = 13,
-	Obsolete = 14,
-	Obsolete2 = 15,
-	ArcaneCharges = 16,
-	Fury = 17,
-	Pain = 18,
-	NumPowerTypes = 19,
-}
+Enum.PowerTypeMeta.NumValues = 22
+Enum.PowerTypeMeta.MinValue = -2
+Enum.PowerTypeMeta.MaxValue = 19
 
-Enum.PowerTypeMeta = {
-	NumValues = 22,
-	MinValue = -2,
-	MaxValue = 19,
-}
+Enum.StoreError.InvalidPaymentMethod = 0
+Enum.StoreError.PaymentFailed = 1
+Enum.StoreError.WrongCurrency = 2
+Enum.StoreError.BattlepayDisabled = 3
+Enum.StoreError.InsufficientBalance = 4
+Enum.StoreError.Other = 5
+Enum.StoreError.AlreadyOwned = 6
+Enum.StoreError.ParentalControlsNoPurchase = 7
+Enum.StoreError.PurchaseDenied = 8
+Enum.StoreError.ConsumableTokenOwned = 9
+Enum.StoreError.TooManyTokens = 10
+Enum.StoreError.ItemUnavailable = 11
 
-Enum.StoreError = {
-	InvalidPaymentMethod = 0,
-	PaymentFailed = 1,
-	WrongCurrency = 2,
-	BattlepayDisabled = 3,
-	InsufficientBalance = 4,
-	Other = 5,
-	AlreadyOwned = 6,
-	ParentalControlsNoPurchase = 7,
-	PurchaseDenied = 8,
-	ConsumableTokenOwned = 9,
-	TooManyTokens = 10,
-	ItemUnavailable = 11,
-}
+Enum.StoreErrorMeta.NumValues = 12
+Enum.StoreErrorMeta.MinValue = 0
+Enum.StoreErrorMeta.MaxValue = 11
 
-Enum.StoreErrorMeta = {
-	NumValues = 12,
-	MinValue = 0,
-	MaxValue = 11,
-}
+Enum.VasError.InvalidDestinationAccount = 6
+Enum.VasError.InvalidSourceAccount = 7
+Enum.VasError.DisallowedSourceAccount = 8
+Enum.VasError.DisallowedDestinationAccount = 9
+Enum.VasError.LowerBoxLevel = 11
+Enum.VasError.RealmNotEligible = 20011
+Enum.VasError.CannotMoveGuildMaster = 20012
+Enum.VasError.MaxCharactersOnServer = 20013
+Enum.VasError.DuplicateCharacterName = 20015
+Enum.VasError.HasMail = 20016
+Enum.VasError.UnderMinLevelReq = 20021
+Enum.VasError.CharacterTransferTooSoon = 20023
+Enum.VasError.CharLocked = 20026
+Enum.VasError.TooMuchMoneyForLevel = 20032
+Enum.VasError.HasAuctions = 20033
+Enum.VasError.LastSaveTooRecent = 20034
+Enum.VasError.NameNotAvailable = 20051
+Enum.VasError.LastRenameTooRecent = 20052
+Enum.VasError.CustomizeAlreadyRequested = 20057
+Enum.VasError.LastCustomizeTooRecent = 20058
+Enum.VasError.FactionChangeTooSoon = 20061
+Enum.VasError.RaceClassComboIneligible = 20062
+Enum.VasError.IneligibleMapID = 20076
+Enum.VasError.BattlepayDeliveryPending = 20078
+Enum.VasError.HasWoWToken = 20079
+Enum.VasError.HasHeirloom = 20080
+Enum.VasError.LastSaveTooDistant = 20083
+Enum.VasError.HasCagedBattlePet = 20084
+Enum.VasError.BoostedTooRecently = 20085
 
-Enum.VasError = {
-	InvalidDestinationAccount = 6,
-	InvalidSourceAccount = 7,
-	DisallowedSourceAccount = 8,
-	DisallowedDestinationAccount = 9,
-	LowerBoxLevel = 11,
-	RealmNotEligible = 20011,
-	CannotMoveGuildMaster = 20012,
-	MaxCharactersOnServer = 20013,
-	DuplicateCharacterName = 20015,
-	HasMail = 20016,
-	UnderMinLevelReq = 20021,
-	CharacterTransferTooSoon = 20023,
-	CharLocked = 20026,
-	TooMuchMoneyForLevel = 20032,
-	HasAuctions = 20033,
-	LastSaveTooRecent = 20034,
-	NameNotAvailable = 20051,
-	LastRenameTooRecent = 20052,
-	CustomizeAlreadyRequested = 20057,
-	LastCustomizeTooRecent = 20058,
-	FactionChangeTooSoon = 20061,
-	RaceClassComboIneligible = 20062,
-	IneligibleMapID = 20076,
-	BattlepayDeliveryPending = 20078,
-	HasWoWToken = 20079,
-	HasHeirloom = 20080,
-	LastSaveTooDistant = 20083,
-	HasCagedBattlePet = 20084,
-	BoostedTooRecently = 20085,
-}
+Enum.VasErrorMeta.NumValues = 29
+Enum.VasErrorMeta.MinValue = 0
+Enum.VasErrorMeta.MaxValue = 20085
 
-Enum.VasErrorMeta = {
-	NumValues = 29,
-	MinValue = 0,
-	MaxValue = 20085,
-}
+Enum.VasPurchaseProgress.Invalid = 0
+Enum.VasPurchaseProgress.PrePurchase = 1
+Enum.VasPurchaseProgress.PaymentPending = 2
+Enum.VasPurchaseProgress.ApplyingLicense = 3
+Enum.VasPurchaseProgress.WaitingOnQueue = 4
+Enum.VasPurchaseProgress.Ready = 5
+Enum.VasPurchaseProgress.ProcessingFactionChange = 6
+Enum.VasPurchaseProgress.Complete = 7
 
-Enum.VasPurchaseProgress = {
-	Invalid = 0,
-	PrePurchase = 1,
-	PaymentPending = 2,
-	ApplyingLicense = 3,
-	WaitingOnQueue = 4,
-	Ready = 5,
-	ProcessingFactionChange = 6,
-	Complete = 7,
-}
+Enum.VasPurchaseProgressMeta.NumValues = 8
+Enum.VasPurchaseProgressMeta.MinValue = 0
+Enum.VasPurchaseProgressMeta.MaxValue = 7
 
-Enum.VasPurchaseProgressMeta = {
-	NumValues = 8,
-	MinValue = 0,
-	MaxValue = 7,
-}
+Enum.VasQueueStatus.UnderAnHour = 0
+Enum.VasQueueStatus.OneToThreeHours = 1
+Enum.VasQueueStatus.ThreeToSixHours = 2
+Enum.VasQueueStatus.SixToTwelveHours = 3
+Enum.VasQueueStatus.OverTwelveHours = 4
+Enum.VasQueueStatus.Over1Days = 5
+Enum.VasQueueStatus.Over2Days = 6
+Enum.VasQueueStatus.Over3Days = 7
+Enum.VasQueueStatus.Over4Days = 8
+Enum.VasQueueStatus.Over5Days = 9
+Enum.VasQueueStatus.Over6Days = 10
+Enum.VasQueueStatus.Over7Days = 11
 
-Enum.VasQueueStatus = {
-	UnderAnHour = 0,
-	OneToThreeHours = 1,
-	ThreeToSixHours = 2,
-	SixToTwelveHours = 3,
-	OverTwelveHours = 4,
-	Over1Days = 5,
-	Over2Days = 6,
-	Over3Days = 7,
-	Over4Days = 8,
-	Over5Days = 9,
-	Over6Days = 10,
-	Over7Days = 11,
-}
+Enum.VasQueueStatusMeta.NumValues = 12
+Enum.VasQueueStatusMeta.MinValue = 0
+Enum.VasQueueStatusMeta.MaxValue = 11
 
-Enum.VasQueueStatusMeta = {
-	NumValues = 12,
-	MinValue = 0,
-	MaxValue = 11,
-}
+Enum.VasServiceType.NameChange = 0
+Enum.VasServiceType.FactionChange = 1
+Enum.VasServiceType.AppearanceChange = 2
+Enum.VasServiceType.RaceChange = 3
+Enum.VasServiceType.CharacterTransfer = 4
+Enum.VasServiceType.FactionTransfer = 5
 
-Enum.VasServiceType = {
-	NameChange = 0,
-	FactionChange = 1,
-	AppearanceChange = 2,
-	RaceChange = 3,
-	CharacterTransfer = 4,
-	FactionTransfer = 5,
-}
-
-Enum.VasServiceTypeMeta = {
-	NumValues = 6,
-	MinValue = 0,
-	MaxValue = 5,
-}
+Enum.VasServiceTypeMeta.NumValues = 6
+Enum.VasServiceTypeMeta.MinValue = 0
+Enum.VasServiceTypeMeta.MaxValue = 5
