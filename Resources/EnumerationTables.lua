bf65ca0f1ffc36cd37f9d292adecc13efee15f71
Resike <resike@gmail.com>
2017-10-31 23:17:22 +0100

    7.3.2 (25383)

--- Resources/EnumerationTables.lua
+++ Resources/EnumerationTables.lua
@@ -1,163 +1,208 @@
-Enum.BattlepayBoostProduct.Level90Boost = 1
-Enum.BattlepayBoostProduct.Level100Boost = 11
-
-Enum.BattlepayBoostProductMeta.NumValues = 2
-Enum.BattlepayBoostProductMeta.MinValue = 0
 Enum.BattlepayBoostProductMeta.MaxValue = 11
+Enum.BattlepayBoostProductMeta.MinValue = 0
+Enum.BattlepayBoostProductMeta.NumValues = 2
 
-Enum.BattlepayProductDecorator.Boost = 0
-Enum.BattlepayProductDecorator.Expansion = 1
-Enum.BattlepayProductDecorator.WoWToken = 2
-Enum.BattlepayProductDecorator.VasService = 3
+Enum.BattlepayDisplayFlagMeta.MaxValue = 2
+Enum.BattlepayDisplayFlagMeta.MinValue = 1
+Enum.BattlepayDisplayFlagMeta.NumValues = 2
 
-Enum.BattlepayProductDecoratorMeta.NumValues = 4
-Enum.BattlepayProductDecoratorMeta.MinValue = 0
+Enum.BattlepayGroupDisplayTypeMeta.MaxValue = 2
+Enum.BattlepayGroupDisplayTypeMeta.MinValue = 0
+Enum.BattlepayGroupDisplayTypeMeta.NumValues = 3
+
 Enum.BattlepayProductDecoratorMeta.MaxValue = 3
+Enum.BattlepayProductDecoratorMeta.MinValue = 0
+Enum.BattlepayProductDecoratorMeta.NumValues = 4
 
-Enum.BattlepaySpecialProducts.Legion = 12
+Enum.BattlepayProductGroupFlagMeta.MaxValue = 4
+Enum.BattlepayProductGroupFlagMeta.MinValue = 1
+Enum.BattlepayProductGroupFlagMeta.NumValues = 3
 
-Enum.BattlepaySpecialProductsMeta.NumValues = 1
-Enum.BattlepaySpecialProductsMeta.MinValue = 0
 Enum.BattlepaySpecialProductsMeta.MaxValue = 12
+Enum.BattlepaySpecialProductsMeta.MinValue = 0
+Enum.BattlepaySpecialProductsMeta.NumValues = 1
 
-Enum.ContributionResult.Success = 0
-Enum.ContributionResult.MustBeNearNpc = 1
-Enum.ContributionResult.IncorrectState = 2
-Enum.ContributionResult.InvalidID = 3
-Enum.ContributionResult.QuestDataMissing = 4
+Enum.BrawlType.Arena = 2
+Enum.BrawlType.Battleground = 1
+Enum.BrawlType.Lfg = 3
+Enum.BrawlType.None = 0
+
+Enum.BrawlTypeMeta.MaxValue = 3
+Enum.BrawlTypeMeta.MinValue = 0
+Enum.BrawlTypeMeta.NumValues = 4
+
+Enum.ConsoleCategory.CategoryCombat = 3
+Enum.ConsoleCategory.CategoryConsole = 2
+Enum.ConsoleCategory.CategoryDebug = 0
+Enum.ConsoleCategory.CategoryDefault = 5
+Enum.ConsoleCategory.CategoryGame = 4
+Enum.ConsoleCategory.CategoryGm = 8
+Enum.ConsoleCategory.CategoryGraphics = 1
+Enum.ConsoleCategory.CategoryNet = 6
+Enum.ConsoleCategory.CategoryNone = 9
+Enum.ConsoleCategory.CategorySound = 7
+
+Enum.ConsoleCategoryMeta.MaxValue = 9
+Enum.ConsoleCategoryMeta.MinValue = 0
+Enum.ConsoleCategoryMeta.NumValues = 10
+
+Enum.ConsoleColorType.AdminColor = 6
+Enum.ConsoleColorType.BackgroundColor = 8
+Enum.ConsoleColorType.ClickbufferColor = 9
+Enum.ConsoleColorType.DefaultColor = 0
+Enum.ConsoleColorType.EchoColor = 2
+Enum.ConsoleColorType.ErrorColor = 3
+Enum.ConsoleColorType.GlobalColor = 5
+Enum.ConsoleColorType.HighlightColor = 7
+Enum.ConsoleColorType.InputColor = 1
+Enum.ConsoleColorType.PrivateColor = 10
+Enum.ConsoleColorType.WarningColor = 4
+
+Enum.ConsoleColorTypeMeta.MaxValue = 10
+Enum.ConsoleColorTypeMeta.MinValue = 0
+Enum.ConsoleColorTypeMeta.NumValues = 11
+
+Enum.ConsoleCommandType.Command = 1
+Enum.ConsoleCommandType.Cvar = 0
+Enum.ConsoleCommandType.Script = 2
+
+Enum.ConsoleCommandTypeMeta.MaxValue = 2
+Enum.ConsoleCommandTypeMeta.MinValue = 0
+Enum.ConsoleCommandTypeMeta.NumValues = 3
+
 Enum.ContributionResult.FailedConditionCheck = 5
-Enum.ContributionResult.UnableToCompleteTurnIn = 6
+Enum.ContributionResult.IncorrectState = 2
 Enum.ContributionResult.InternalError = 7
+Enum.ContributionResult.InvalidID = 3
+Enum.ContributionResult.MustBeNearNpc = 1
+Enum.ContributionResult.QuestDataMissing = 4
+Enum.ContributionResult.Success = 0
+Enum.ContributionResult.UnableToCompleteTurnIn = 6
 
-Enum.ContributionResultMeta.NumValues = 8
-Enum.ContributionResultMeta.MinValue = 0
 Enum.ContributionResultMeta.MaxValue = 7
+Enum.ContributionResultMeta.MinValue = 0
+Enum.ContributionResultMeta.NumValues = 8
 
-Enum.ContributionState.None = 0
-Enum.ContributionState.Building = 1
 Enum.ContributionState.Active = 2
-Enum.ContributionState.UnderAttack = 3
+Enum.ContributionState.Building = 1
 Enum.ContributionState.Destroyed = 4
+Enum.ContributionState.None = 0
+Enum.ContributionState.UnderAttack = 3
 
-Enum.ContributionStateMeta.NumValues = 5
-Enum.ContributionStateMeta.MinValue = 0
 Enum.ContributionStateMeta.MaxValue = 4
+Enum.ContributionStateMeta.MinValue = 0
+Enum.ContributionStateMeta.NumValues = 5
 
-Enum.PowerType.HealthCost = -2
-Enum.PowerType.None = -1
-Enum.PowerType.Mana = 0
-Enum.PowerType.Rage = 1
-Enum.PowerType.Focus = 2
-Enum.PowerType.Energy = 3
+Enum.MapCanvasPosition.BottomLeft = 0
+Enum.MapCanvasPosition.BottomRight = 1
+Enum.MapCanvasPosition.TopLeft = 2
+Enum.MapCanvasPosition.TopRight = 3
+
+Enum.MapCanvasPositionMeta.MaxValue = 3
+Enum.MapCanvasPositionMeta.MinValue = 0
+Enum.MapCanvasPositionMeta.NumValues = 4
+
+Enum.MapTransform.IsForFlightMap = 1
+
+Enum.MapTransformMeta.MaxValue = 1
+Enum.MapTransformMeta.MinValue = 1
+Enum.MapTransformMeta.NumValues = 1
+
+Enum.ModelSceneType.ArtifactRelicTalentEffect = 9
+Enum.ModelSceneType.ArtifactTier2 = 5
+Enum.ModelSceneType.ArtifactTier2ForgingScene = 6
+Enum.ModelSceneType.ArtifactTier2SlamEffect = 7
+Enum.ModelSceneType.CommentatorVictoryFanfare = 8
+Enum.ModelSceneType.EncounterJournal = 3
+Enum.ModelSceneType.MountJournal = 0
+Enum.ModelSceneType.PetJournalCard = 1
+Enum.ModelSceneType.PetJournalLoadout = 4
+Enum.ModelSceneType.ShopCard = 2
+
+Enum.ModelSceneTypeMeta.MaxValue = 9
+Enum.ModelSceneTypeMeta.MinValue = 0
+Enum.ModelSceneTypeMeta.NumValues = 10
+
+Enum.PowerType.Alternate = 10
+Enum.PowerType.ArcaneCharges = 16
+Enum.PowerType.Chi = 12
 Enum.PowerType.ComboPoints = 4
+Enum.PowerType.Energy = 3
+Enum.PowerType.Focus = 2
+Enum.PowerType.Fury = 17
+Enum.PowerType.HealthCost = -2
+Enum.PowerType.HolyPower = 9
+Enum.PowerType.Insanity = 13
+Enum.PowerType.LunarPower = 8
+Enum.PowerType.Maelstrom = 11
+Enum.PowerType.Mana = 0
+Enum.PowerType.None = -1
+Enum.PowerType.NumPowerTypes = 19
+Enum.PowerType.Obsolete = 14
+Enum.PowerType.Obsolete2 = 15
+Enum.PowerType.Pain = 18
+Enum.PowerType.Rage = 1
 Enum.PowerType.Runes = 5
 Enum.PowerType.RunicPower = 6
 Enum.PowerType.SoulShards = 7
-Enum.PowerType.LunarPower = 8
-Enum.PowerType.HolyPower = 9
-Enum.PowerType.Alternate = 10
-Enum.PowerType.Maelstrom = 11
-Enum.PowerType.Chi = 12
-Enum.PowerType.Insanity = 13
-Enum.PowerType.Obsolete = 14
-Enum.PowerType.Obsolete2 = 15
-Enum.PowerType.ArcaneCharges = 16
-Enum.PowerType.Fury = 17
-Enum.PowerType.Pain = 18
-Enum.PowerType.NumPowerTypes = 19
 
-Enum.PowerTypeMeta.NumValues = 22
-Enum.PowerTypeMeta.MinValue = -2
 Enum.PowerTypeMeta.MaxValue = 19
+Enum.PowerTypeMeta.MinValue = -2
+Enum.PowerTypeMeta.NumValues = 22
 
-Enum.StoreError.InvalidPaymentMethod = 0
-Enum.StoreError.PaymentFailed = 1
-Enum.StoreError.WrongCurrency = 2
-Enum.StoreError.BattlepayDisabled = 3
-Enum.StoreError.InsufficientBalance = 4
-Enum.StoreError.Other = 5
-Enum.StoreError.AlreadyOwned = 6
-Enum.StoreError.ParentalControlsNoPurchase = 7
-Enum.StoreError.PurchaseDenied = 8
-Enum.StoreError.ConsumableTokenOwned = 9
-Enum.StoreError.TooManyTokens = 10
-Enum.StoreError.ItemUnavailable = 11
+Enum.StoreDeliveryType.Battlepet = 2
+Enum.StoreDeliveryType.Collection = 3
+Enum.StoreDeliveryType.Item = 0
+Enum.StoreDeliveryType.Mount = 1
 
-Enum.StoreErrorMeta.NumValues = 12
-Enum.StoreErrorMeta.MinValue = 0
+Enum.StoreDeliveryTypeMeta.MaxValue = 3
+Enum.StoreDeliveryTypeMeta.MinValue = 0
+Enum.StoreDeliveryTypeMeta.NumValues = 4
+
 Enum.StoreErrorMeta.MaxValue = 11
+Enum.StoreErrorMeta.MinValue = 0
+Enum.StoreErrorMeta.NumValues = 12
 
-Enum.VasError.InvalidDestinationAccount = 6
-Enum.VasError.InvalidSourceAccount = 7
-Enum.VasError.DisallowedSourceAccount = 8
-Enum.VasError.DisallowedDestinationAccount = 9
-Enum.VasError.LowerBoxLevel = 11
-Enum.VasError.RealmNotEligible = 20011
-Enum.VasError.CannotMoveGuildMaster = 20012
-Enum.VasError.MaxCharactersOnServer = 20013
-Enum.VasError.DuplicateCharacterName = 20015
-Enum.VasError.HasMail = 20016
-Enum.VasError.UnderMinLevelReq = 20021
-Enum.VasError.CharacterTransferTooSoon = 20023
-Enum.VasError.CharLocked = 20026
-Enum.VasError.TooMuchMoneyForLevel = 20032
-Enum.VasError.HasAuctions = 20033
-Enum.VasError.LastSaveTooRecent = 20034
-Enum.VasError.NameNotAvailable = 20051
-Enum.VasError.LastRenameTooRecent = 20052
-Enum.VasError.CustomizeAlreadyRequested = 20057
-Enum.VasError.LastCustomizeTooRecent = 20058
-Enum.VasError.FactionChangeTooSoon = 20061
-Enum.VasError.RaceClassComboIneligible = 20062
-Enum.VasError.IneligibleMapID = 20076
-Enum.VasError.BattlepayDeliveryPending = 20078
-Enum.VasError.HasWoWToken = 20079
-Enum.VasError.HasHeirloom = 20080
-Enum.VasError.LastSaveTooDistant = 20083
-Enum.VasError.HasCagedBattlePet = 20084
-Enum.VasError.BoostedTooRecently = 20085
+Enum.TransmogSource.Achievement = 7
+Enum.TransmogSource.CantCollect = 6
+Enum.TransmogSource.HiddenUntilCollected = 5
+Enum.TransmogSource.JournalEncounter = 1
+Enum.TransmogSource.None = 0
+Enum.TransmogSource.NotValidForTransmog = 9
+Enum.TransmogSource.Profession = 8
+Enum.TransmogSource.Quest = 2
+Enum.TransmogSource.Vendor = 3
+Enum.TransmogSource.WorldDrop = 4
 
-Enum.VasErrorMeta.NumValues = 29
-Enum.VasErrorMeta.MinValue = 0
+Enum.TransmogSourceMeta.MaxValue = 9
+Enum.TransmogSourceMeta.MinValue = 0
+Enum.TransmogSourceMeta.NumValues = 10
+
 Enum.VasErrorMeta.MaxValue = 20085
+Enum.VasErrorMeta.MinValue = 0
+Enum.VasErrorMeta.NumValues = 29
 
-Enum.VasPurchaseProgress.Invalid = 0
-Enum.VasPurchaseProgress.PrePurchase = 1
-Enum.VasPurchaseProgress.PaymentPending = 2
 Enum.VasPurchaseProgress.ApplyingLicense = 3
-Enum.VasPurchaseProgress.WaitingOnQueue = 4
-Enum.VasPurchaseProgress.Ready = 5
-Enum.VasPurchaseProgress.ProcessingFactionChange = 6
 Enum.VasPurchaseProgress.Complete = 7
+Enum.VasPurchaseProgress.Invalid = 0
+Enum.VasPurchaseProgress.PaymentPending = 2
+Enum.VasPurchaseProgress.PrePurchase = 1
+Enum.VasPurchaseProgress.ProcessingFactionChange = 6
+Enum.VasPurchaseProgress.Ready = 5
+Enum.VasPurchaseProgress.WaitingOnQueue = 4
 
-Enum.VasPurchaseProgressMeta.NumValues = 8
-Enum.VasPurchaseProgressMeta.MinValue = 0
 Enum.VasPurchaseProgressMeta.MaxValue = 7
+Enum.VasPurchaseProgressMeta.MinValue = 0
+Enum.VasPurchaseProgressMeta.NumValues = 8
 
-Enum.VasQueueStatus.UnderAnHour = 0
-Enum.VasQueueStatus.OneToThreeHours = 1
-Enum.VasQueueStatus.ThreeToSixHours = 2
-Enum.VasQueueStatus.SixToTwelveHours = 3
-Enum.VasQueueStatus.OverTwelveHours = 4
-Enum.VasQueueStatus.Over1Days = 5
-Enum.VasQueueStatus.Over2Days = 6
-Enum.VasQueueStatus.Over3Days = 7
-Enum.VasQueueStatus.Over4Days = 8
-Enum.VasQueueStatus.Over5Days = 9
-Enum.VasQueueStatus.Over6Days = 10
-Enum.VasQueueStatus.Over7Days = 11
-
-Enum.VasQueueStatusMeta.NumValues = 12
-Enum.VasQueueStatusMeta.MinValue = 0
-Enum.VasQueueStatusMeta.MaxValue = 11
-
-Enum.VasServiceType.NameChange = 0
-Enum.VasServiceType.FactionChange = 1
-Enum.VasServiceType.AppearanceChange = 2
-Enum.VasServiceType.RaceChange = 3
-Enum.VasServiceType.CharacterTransfer = 4
-Enum.VasServiceType.FactionTransfer = 5
-
-Enum.VasServiceTypeMeta.NumValues = 6
-Enum.VasServiceTypeMeta.MinValue = 0
 Enum.VasServiceTypeMeta.MaxValue = 5
+Enum.VasServiceTypeMeta.MinValue = 0
+Enum.VasServiceTypeMeta.NumValues = 6
+
+Enum.ZoneAbilityType.Argus = 2
+Enum.ZoneAbilityType.Garrison = 0
+Enum.ZoneAbilityType.OrderHall = 1
+
+Enum.ZoneAbilityTypeMeta.MaxValue = 2
+Enum.ZoneAbilityTypeMeta.MinValue = 0
+Enum.ZoneAbilityTypeMeta.NumValues = 3
